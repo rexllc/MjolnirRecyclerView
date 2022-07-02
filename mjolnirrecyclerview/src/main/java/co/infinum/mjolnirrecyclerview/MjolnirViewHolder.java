8038b913c3580ba1b3b2d2932a9dfca790052bd7
Rex Luciano <64059802+rexllc@users.noreply.github.com>
2022-07-03 05:54:00 +0800

    Update to AndroidX

--- mjolnirrecyclerview/src/main/java/co/infinum/mjolnirrecyclerview/MjolnirViewHolder.java
+++ mjolnirrecyclerview/src/main/java/co/infinum/mjolnirrecyclerview/MjolnirViewHolder.java
@@ -1,6 +1,6 @@
 package co.infinum.mjolnirrecyclerview;
 
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.View;
 
 import java.util.List;
